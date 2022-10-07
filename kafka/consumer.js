d725d83e764b73e508c6f55b742f9c5e2972f98e
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2022-10-07 20:54:00 +0530

    add logger

--- kafka/consumer.js
+++ kafka/consumer.js
@@ -1,4 +1,4 @@
-const { Kafka } = require('kafkajs');
+const { Kafka, logLevel } = require('kafkajs');
 
 const brokers = ['localhost:9092'];
 
@@ -7,6 +7,7 @@ async function run() {
     const kafka = new Kafka({
       clientId: 'my_app',
       brokers,
+      logLevel: logLevel.ERROR,
     });
 
     const consumer = kafka.consumer({ groupId: 'test' });
@@ -20,47 +21,54 @@ async function run() {
     });
 
     await consumer.run({
-      // eachMessage: async (result) => {
-      //   console.log({
-      //     result,
-      //     message: result.message.value,
-      //     partition: result.partition,
-      //   });
-      // },
-
-      eachBatchAutoResolve: true,
-      eachBatch: async ({
-        batch,
-        resolveOffset,
-        heartbeat,
-        commitOffsetsIfNecessary,
-        uncommittedOffsets,
-        isRunning,
-        isStale,
-        pause,
-      }) => {
-        for (let message of batch.messages) {
-          console.log({
-            topic: batch.topic,
-            partition: batch.partition,
-            highWatermark: batch.highWatermark,
-            message: {
-              offset: message.offset,
-              key: message.key?.toString(),
-              value: message.value?.toString(),
-              headers: message.headers,
-            },
-            commitOffsetsIfNecessary,
-            uncommittedOffsets,
-            isRunning,
-            isStale,
-            pause,
-          });
-
-          resolveOffset(message.offset);
-          await heartbeat();
-        }
+      partitionsConsumedConcurrently: 2, // Default: 1
+      eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
+        console.log({
+          topic,
+          partition,
+          messageText: message.value.toString(),
+          message,
+          heartbeat,
+          pause,
+        });
       },
+
+      // ==========================================
+      // To read messages in batch
+      // ==========================================
+
+      // eachBatchAutoResolve: true,
+      // eachBatch: async ({
+      //   batch,
+      //   resolveOffset,
+      //   heartbeat,
+      //   commitOffsetsIfNecessary,
+      //   uncommittedOffsets,
+      //   isRunning,
+      //   isStale,
+      //   pause,
+      // }) => {
+      //   for (let message of batch.messages) {
+      //     console.log({
+      //       topic: batch.topic,
+      //       partition: batch.partition,
+      //       highWatermark: batch.highWatermark,
+      //       message: {
+      //         offset: message.offset,
+      //         key: message.key?.toString(),
+      //         value: message.value?.toString(),
+      //         headers: message.headers,
+      //       },
+      //       commitOffsetsIfNecessary,
+      //       uncommittedOffsets,
+      //       isRunning,
+      //       isStale,
+      //       pause,
+      //     });
+      //     resolveOffset(message.offset);
+      //     await heartbeat();
+      //   }
+      // },
     });
   } catch (err) {
     console.error('Something bad happened', err);
